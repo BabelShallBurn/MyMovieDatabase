75be2e145595dffd3ff17db591c0f4803d5dcd60
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-09-30 15:05:23 +0200

    added models for the tables users and movies

---
+++ models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    def __init__(self, username):
+        self.username = username
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    def __init__(self, title, genre, name, director, year, poster_url=None):
+        self.title = title
+        self.genre = genre
+        self.name = name
+        self.director = director
+        self.year = year
+        self.poster_url = poster_url
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    poster_url = db.Column(db.String(300), nullable=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
