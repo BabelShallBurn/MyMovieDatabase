54ac77831ae19f418726e82944aaebe1388bfbbb
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-10-02 16:00:43 +0200

    added error handling if page is not found

--- app.py
+++ app.py
@@ -137,6 +137,10 @@ def delete_movie(user_id, movie_id):
     return redirect(url_for('list_movies', user_id=user_id))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
   with app.app_context():
     db.create_all()
